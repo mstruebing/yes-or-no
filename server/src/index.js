1b4a7cad98ed0bd2e36ed42eb06008429f55399e
Max Strübing <mxstrbng@gmail.com>
2018-04-30 07:15:42 +0200

    chore: project-setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 
+import sum from './sum';
+
 const app = express();
 
+console.log(sum(1, 2));
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('Hello World!!!');
 });
+
+app.listen(3000);
