12da93c7cd1ff6e9c9b566a1858f2a24c13cfc93
Max Strübing <mxstrbng@gmail.com>
2018-05-22 05:41:15 +0200

    chore: add nodemon and watching

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ import {isUser, addUser} from './user';
 const app = express();
 
 const allowCrossDomain = (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://local:3000');
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 	res.header('Access-Control-Allow-Credentials', true);
